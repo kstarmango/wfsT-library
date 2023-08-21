f835c04188242385c5c893d3667ebbd2dd19b03c
LAPTOP-VD9MQT70\gurwl <121783220+kstarmango@users.noreply.github.com>
2023-08-21 15:32:14 +0900

    readme 수정

--- README.md
+++ README.md
@@ -1,7 +1,33 @@
 # wfs-Transaction
+*See also https://github.com/kstarmango/wfsT-library*
 
-*See also https://github.com/rollup/rollup-starter-lib*
+## Usage
 
+```
+// 1. set GML option (it's shared below functions)
+const t = new WfsTransaction(featureNS, featureType, srsName)
+
+// 2. if single transact call
+t.submitTransact(mode, feature, layerName)
+
+// 2. if multi transact call
+
+  // editedItems set 1. 
+  t.setEditedItems(editItems)
+  // editedItems set 2. 
+  t.addEditedItems(mode, feature, layerName)
+
+  // after setting
+  t.submitMultiTransact()
+
+  // if editedItems are written manually, 
+  t.submitMultiTransact(editedItems_)
+
+// 3. data cleanup
+const res = t.setEditedItems();
+if(res){
+  cleanup();
+}
+```
 ## License
-
 [MIT](LICENSE).
--- src/transaction.js
+++ src/transaction.js
@@ -9,178 +9,177 @@ Create wfs-transaction call for OpenLayers
 */
 
 import { WFS, GML } from 'ol/format';
-import axios from "axios";
+import axios from 'axios';
 import dom2str from 'dom-to-string';
 import parseXML from 'xml-parse-from-string';
 
 export default class WfsTransaction {
-  
-  /**
-   * @param {options} options - url, featureNS, srsName
-   */
-  constructor(options){
+	/**
+	 * @param {options} options - url, featureNS, srsName
+	 */
+	constructor(options) {
+		/**
+		 * @type {string}
+		 * @private
+		 */
+		this.featureNS = options.featureNS;
 
-    /**
-     * @type {string}
-     * @private
-     */
-    this.featureNS = options.featureNS
+		/**
+		 * @type {string}
+		 * @private
+		 */
+		this.srsName = options.srsName;
 
-    /**
-     * @type {string}
-     * @private
-     */
-    this.srsName = options.srsName
+		/**
+		 * @type {string}
+		 * @private
+		 */
+		this.url =
+			options.url +
+			'wfs?dataType=xml&processData=false&contentType=text/xml&version=1.1.0&request=transaction';
 
-    /**
-     * @type {string}
-     * @private
-     */
-    this.url = options.url + 
-      'wfs?dataType=xml&processData=false&contentType=text/xml&version=1.1.0&request=transaction'
+		/**
+		 * @type {Array}
+		 * @private
+		 */
+		this.editedItems = [];
+	}
 
-    /**
-     * @type {Array}
-     * @private
-     */
-    this.editedItems = [];
+	/**
+	 * transact call create and serialize
+	 * @param {string} mode - insert, update, delete
+	 * @param {feature} feature
+	 * @param {layerName} layerName
+	 */
+	createTransact(mode, feature, layerName) {
+		if (feature.getGeometryName() !== 'geom') {
+			feature.setGeometryName('geom');
+		}
 
-  }
+		const formatWFS = new WFS();
 
-  /**
-   * transact call create and serialize
-   * @param {string} mode - insert, update, delete
-   * @param {feature} feature 
-   * @param {layerName} layerName 
-   */
-  createTransact(mode, feature, layerName){
+		let formatGML = new GML({
+			featureNS: this.featureNS,
+			featureType: layerName,
+			srsName: this.srsName,
+		});
 
-    if(feature.getGeometryName() !== "geom"){
-      feature.setGeometryName("geom");
-    }
+		let node = null;
 
-    const formatWFS = new WFS();
-  
-    let formatGML = new GML({
-      featureNS: this.featureNS,
-      featureType: layerName,
-      srsName: this.srsName,
-    });
-  
-    let node = null;
-  
-    switch (mode) {
-      case 'insert':
-          node = formatWFS.writeTransaction([feature], null, null, formatGML);
-        break;
-      case 'update':
-          node = formatWFS.writeTransaction(null, [feature], null, formatGML);
-        break;
-      case 'delete':
-          node = formatWFS.writeTransaction(null, null, [feature], formatGML);
-        break;
-      default: break;
-    }
+		switch (mode) {
+			case 'insert':
+				node = formatWFS.writeTransaction([feature], null, null, formatGML);
+				break;
+			case 'update':
+				node = formatWFS.writeTransaction(null, [feature], null, formatGML);
+				break;
+			case 'delete':
+				node = formatWFS.writeTransaction(null, null, [feature], formatGML);
+				break;
+			default:
+				break;
+		}
 
-    const dataStr = new XMLSerializer().serializeToString(node);
+		const dataStr = new XMLSerializer().serializeToString(node);
 
-    return dataStr;
-  }
-  
-  /**
-   * single transact call
-   * @param {string} mode - insert, update, delete
-   * @param {feature} feature 
-   * @param {layerName} layerName 
-   */
-  submitTransact(mode, feature, layerName){
-    const createWfstXml = this.createTransact(mode, feature, layerName);
-    return this.post(createWfstXml);
-  }
-  
-  setEditedItems(editItems){
-    return this.editedItems = editItems;
-  }
+		return dataStr;
+	}
 
-  addEditedItems(mode, feature, layerName){
-    return this.editedItems.push({ 
-      mode : mode, 
-      feature : feature, 
-      layerName : layerName 
-    })
-  }
-  
-  /** 
-   * @param {editedItems_} {[ mode, feature, layerName ]} 
-   * @return post res
-   */
-  submitMultiTransact(editedItems_){
-    
-    let editedItems;
+	/**
+	 * single transact call
+	 * @param {string} mode - insert, update, delete
+	 * @param {feature} feature
+	 * @param {layerName} layerName
+	 */
+	submitTransact(mode, feature, layerName) {
+		const createWfstXml = this.createTransact(mode, feature, layerName);
+		return this.post(createWfstXml);
+	}
 
-    if(this.editedItems){
-      editedItems = this.editedItems;
-    } else if(editedItems_) {
-      editedItems = editedItems_;
-    } 
+	setEditedItems(editItems) {
+		return (this.editedItems = editItems);
+	}
 
-    let convertWfstXml = null;
-    const layerNames = [];
-  
-    for (let item of editedItems) {
-      if (!layerNames.includes(item.layerName)){
-        layerNames.push(item.layerName);
-      }
-    }
-  
-    for (let i = 0; i < layerNames.length; i++) {
-      const edited = editedItems.filter(
-        (item) => item.layerName === layerNames[i]
-      );
-  
-      for (let item of edited) {
-        const createWfstXml = this.createTransactWFS(
-          item.mode,
-          item.feature,
-          item.layerName
-        );
-       
-        let typeWfstXml = parseXML(createWfstXml);
-  
-        if (convertWfstXml === null) {
-          convertWfstXml = typeWfstXml;
-        } else {
-          let selectWfstXml = null;
-          if (item.type === 'delete') {
-            selectWfstXml = typeWfstXml.getElementsByTagName('Delete')[0];
-          } else {
-            if (item.type === 'insert') {
-              selectWfstXml = typeWfstXml.getElementsByTagName('Insert')[0];
-            } else {
-              selectWfstXml = typeWfstXml.getElementsByTagName('Update')[0];
-            }
-          }
-          convertWfstXml.children[0].append(selectWfstXml);
-        }
-      }
-    }
+	addEditedItems(mode, feature, layerName) {
+		return this.editedItems.push({
+			mode: mode,
+			feature: feature,
+			layerName: layerName,
+		});
+	}
 
-    let requestWfstXml = dom2str(convertWfstXml.children[0]);
-    const res = this.post(requestWfstXml);
-    if(res){
-      this.editedItems = [];
-      return res;
-    }
-  }
+	/**
+	 * @param {editedItems_} {[ mode, feature, layerName ]}
+	 * @return post res
+	 */
+	submitMultiTransact(editedItems_) {
+		let editedItems;
 
-  post(requestWfstXml){
-    axios
-      .post(this.url, requestWfstXml, { headers: { 'Content-Type': 'text/xml' } })
-      .then((res) => {
-        return res
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  }
+		if (this.editedItems) {
+			editedItems = this.editedItems;
+		} else if (editedItems_) {
+			editedItems = editedItems_;
+		}
+
+		let convertWfstXml = null;
+		const layerNames = [];
+
+		for (let item of editedItems) {
+			if (!layerNames.includes(item.layerName)) {
+				layerNames.push(item.layerName);
+			}
+		}
+
+		for (let i = 0; i < layerNames.length; i++) {
+			const edited = editedItems.filter(
+				(item) => item.layerName === layerNames[i]
+			);
+
+			for (let item of edited) {
+				const createWfstXml = this.createTransactWFS(
+					item.mode,
+					item.feature,
+					item.layerName
+				);
+
+				let typeWfstXml = parseXML(createWfstXml);
+
+				if (convertWfstXml === null) {
+					convertWfstXml = typeWfstXml;
+				} else {
+					let selectWfstXml = null;
+					if (item.type === 'delete') {
+						selectWfstXml = typeWfstXml.getElementsByTagName('Delete')[0];
+					} else {
+						if (item.type === 'insert') {
+							selectWfstXml = typeWfstXml.getElementsByTagName('Insert')[0];
+						} else {
+							selectWfstXml = typeWfstXml.getElementsByTagName('Update')[0];
+						}
+					}
+					convertWfstXml.children[0].append(selectWfstXml);
+				}
+			}
+		}
+
+		let requestWfstXml = dom2str(convertWfstXml.children[0]);
+		const res = this.post(requestWfstXml);
+		if (res) {
+			this.editedItems = [];
+			return res;
+		}
+	}
+
+	post(requestWfstXml) {
+		axios
+			.post(this.url, requestWfstXml, {
+				headers: { 'Content-Type': 'text/xml' },
+			})
+			.then((res) => {
+				return res;
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+	}
 }
