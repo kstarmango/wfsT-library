c3b052152da49111075b987d628a514ac417f944
LAPTOP-VD9MQT70\gurwl <121783220+kstarmango@users.noreply.github.com>
2023-08-21 14:45:37 +0900

    transaction npm 배포버전

--- rollup.config.js
+++ rollup.config.js
@@ -1,21 +1,29 @@
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import { terser } from 'rollup-plugin-terser';
+import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 
-// `npm run build` -> `production` is true
-// `npm run dev` -> `production` is false
 const production = !process.env.ROLLUP_WATCH;
 
 export default {
 	input: 'src/main.js',
 	output: {
 		file: 'public/bundle.js',
-		format: 'iife', // immediately-invoked function expression — suitable for <script> tags
-		sourcemap: true
+		format: 'es', // immediately-invoked function expression — suitable for <script> tags
+		sourcemap: true,
+		external: [
+			"ol"
+		]
 	},
 	plugins: [
 		resolve(), // tells Rollup how to find date-fns in node_modules
-		commonjs(), // converts date-fns to ES modules
-		production && terser() // minify, but only in production
+		commonjs({ // CommonJS 로 작성된 모듈들을 ES6 바꾸어서 rollup이 해석할 수 있게 도와줍니다.
+			extensions: ['.js'],
+		}), // converts date-fns to ES modules
+		production && terser(), // minify, but only in production
+		peerDepsExternal(),
 	]
 };
+
+// `npm run build` -> `production` is true
+// `npm run dev` -> `production` is false
--- src/main.js
+++ src/main.js
@@ -1,7 +1,3 @@
-import update from './update.js';
+import WfsTransaction from './transaction'
 
-// even though Rollup is bundling all your files together, errors and
-// logs will still point to your original source modules
-console.log('if you have sourcemaps enabled in your devtools, click on main.js:5 -->');
-
-update();
+export default WfsTransaction
